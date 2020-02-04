064ba11691a00b4490884991ed25ef2213c5cd55
Turkey <turkey2349@gmail.com>
2020-02-04 13:54:11 -0500

    Updated to use better searching

--- PublicTrelloFinder/src/TrelloScrape.java
+++ PublicTrelloFinder/src/TrelloScrape.java
@@ -1,25 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class TrelloScrape {
+	private static final char[] chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+
 	public static void main(String[] args) {
+		List<String> urls = new ArrayList<>();
 		int start = 0;
-		while (true) {
-			Document doc;
-			try {
-				doc = Jsoup.connect("https://www.google.com/search?q=site:trello.com/b/&start=" + start).get();
-				Elements results = doc.getElementsByClass("g");
-				start += results.size();
-				for (int i = 0; i < results.size(); i++) {
-					Element result = results.get(i).getElementsByClass("r").first();
-					String link = result.getElementsByTag("a").first().attr("href");
-					String name = result.getElementsByTag("a").first().getElementsByTag("h3").first().html();
-					System.out.println(name + "  " + link);
+		for (char c : chars) {
+			boolean done = false;
+			start = 0;
+			while (!done) {
+				Document doc;
+				try {
+
+					String url = "https://www.google.com/search?q=" + String.valueOf(c) + "+site:trello.com/b/&start="
+							+ start;
+					doc = Jsoup.connect(url).get();
+					Elements results = doc.getElementsByClass("g");
+					if (results.size() == 0) {
+						done = true;
+						continue;
+					}
+					start += results.size();
+					for (int i = 0; i < results.size(); i++) {
+						Element result = results.get(i).getElementsByClass("r").first();
+						String link = result.getElementsByTag("a").first().attr("href");
+						String name = result.getElementsByTag("a").first().getElementsByTag("h3").first().html();
+						if (!urls.contains(link)) {
+							urls.add(link);
+							System.out.println(name + "  " + link);
+						}
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+					continue;
 				}
-			} catch (Exception e) {
-				continue;
 			}
 		}
 	}
